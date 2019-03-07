c00540531bef334f18a4ce20e07b7c3a52f0b8bd
Duke Lu <23153285987@qq.com>
2019-03-08 01:12:14 +0800

    初始化项目

---
+++ src/main/java/com/idukelu/demo/demofeignclient/DemoFeignClientApplication.java
@@ -0,0 +1,13 @@
+package com.idukelu.demo.demofeignclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoFeignClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoFeignClientApplication.class, args);
+    }
+
+}
