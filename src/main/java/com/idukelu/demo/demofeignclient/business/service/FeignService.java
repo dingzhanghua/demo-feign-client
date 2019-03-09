d61b3b89a69b649fe63bb66d3f6aa0171fc8dbf3
Duke Lu <23153285987@qq.com>
2019-03-09 15:59:43 +0800

    添加熔断器

--- src/main/java/com/idukelu/demo/demofeignclient/business/service/FeignService.java
+++ src/main/java/com/idukelu/demo/demofeignclient/business/service/FeignService.java
@@ -5,7 +5,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-@FeignClient("demo-eureka-client")
+@FeignClient(value = "demo-eureka-client", fallback = FeignServiceFallback.class)
 public interface FeignService {
 
     @PostMapping("/eurekasvr")
@@ -20,4 +20,23 @@ public interface FeignService {
     @GetMapping(value = "/eurekasvr")
     ResponseEntity<?> getUser(@RequestParam(value = "id", required = false) String id,
                               @RequestParam(value = "username", required = false) String username) throws Exception;
+}
+
+class FeignServiceFallback implements FeignService{
+
+    public ResponseEntity<?> createUser(User user) throws Exception {
+        return ResponseEntity.ok("创建用户失败！！！");
+    }
+
+    public ResponseEntity<?> deleteUser(String id) throws Exception {
+        return ResponseEntity.ok("删除用户失败！！！");
+    }
+
+    public ResponseEntity<?> updateUser(User user) throws Exception {
+        return ResponseEntity.ok("更新用户失败！！！");
+    }
+
+    public ResponseEntity<?> getUser(String id, String username) throws Exception {
+        return ResponseEntity.ok("查询用户失败！！！");
+    }
 }
